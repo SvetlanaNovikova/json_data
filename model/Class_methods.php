0d8bb8393352ab32896db7b2a7548a947436ce48
Арина <8vedima8@gmail.com>
2019-11-11 23:26:49 +0300

    Added the add_post function and a small fix

--- model/Class_methods.php
+++ model/Class_methods.php
@@ -4,16 +4,17 @@ use name\Configuration;
 
 class Class_methods
 {
-    function Main($type, $data){
+    function Main($type, $data, $method){
         switch($type){
-            case  "get_domains": $this->get_domains(); break;
+            case  "get_domains": $this->get_domains($method); break;
             case  "add_domains": $this->add_domains($data); break;
+            case  "add_post": $this->add_post($data); break;
         }
     }
     /*получение всех доменов из бд*/
-    function get_domains()
+    function get_domains($method)
     {
-        $result = file_get_contents(Configuration::$url.'domains', false);
+        $result = file_get_contents(Configuration::$url.$method, false);
         return $result;
     }
     /*создание нового домена*/
@@ -22,6 +23,32 @@ class Class_methods
         $data = array("domainName" => $data["domainName"]);
         $data_string = json_encode($data);
         $rezult = $this->post_execute($data_string, 'domains');
+        if (!empty( $rezult["id"])){
+            return true;
+        }
+        else return false;
+    }
+
+    /*создание нового поста*/
+    function add_post($data)
+    {
+        //новое id поста (НУЖНО ПОМЕНЯТЬ, ТУТ ВОЗВРАЩАЕТСЯ ЛИМИТ)
+        $ch = curl_init(Configuration::$url.'domains/'.$data["id"].'/post/all');
+        //убрать все кроме цифр
+        $ch = preg_replace('~[^0-9]+~','',$ch);
+
+        $data = array("title" => $data["title"],
+            "content"=> $data["content"],
+            "author"=> $data["author"],
+            "excerpt"=> $data["excerpt"],
+            "status"=> $data["status"],
+            "postParent"=> $data["id"],
+            "url"=> Configuration::$url.'domains/'.$data["id"].'/post/'.$ch,
+            "thumbnailUrl"=> $data["content"],
+            "postType"=> $data["postType"]);
+
+        $data_string = json_encode($data);
+        $rezult = $this->post_execute($data_string, 'domains/'.$data["id"].'/post');
         if (!empty( $rezult["id"])){
             return true;
         }
--- test.php
+++ test.php
@@ -1,28 +1,94 @@
 <?php
 include 'config.php';
+use name\Configuration;
 
-$rezult = [];
-$rezult['domainName'] = 'Test domain 2';
-$rezult = json_encode($rezult);
-
-$data = array("domainName" => "Test domain 2000");
-$data_string = json_encode($data);
-
-$ch = curl_init('http://vulture-tm.ru:3000/domains ');
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Content-Type: application/json',
-        'Content-Length: ' . strlen($data_string))
-);
-$result = curl_exec($ch);
-var_dump($result);
-var_dump('    ');
-$data = json_decode($result, true);
-var_dump($data);
-if (!empty( $data["id"])){
-    echo 'OK';
+function get_domains($method)
+{
+    $result = file_get_contents(Configuration::$url.$method, false);
+    return $result;
 }
-else echo '((';
-return $data;
+
+/*создание нового домена*/
+function add_domains($data)
+{
+    $data = array("domainName" => $data["domainName"]);
+    //$data = array("domainName" => $data["domainName"]);
+    $data_string = json_encode($data);
+    $rezult = $this->post_execute($data_string, 'domains');
+    if (!empty( $rezult["id"])){
+        return true;
+    }
+    else return false;
+}
+
+/*создание нового поста*/
+function add_post($data)
+{
+    //новое id поста (НУЖНО ПОМЕНЯТЬ, ТУТ ВОЗВРАЩАЕТСЯ ЛИМИТ)
+    $ch = curl_init(Configuration::$url.'domains/'.$data["id"].'/post/all');
+    //убрать все кроме цифр
+    $ch = preg_replace('~[^0-9]+~','',$ch);
+
+    $data = array("title" => $data["title"],
+        "content"=> $data["content"],
+        "author"=> $data["author"],
+        "excerpt"=> $data["excerpt"],
+        "status"=> $data["status"],
+        "postParent"=> $data["id"],
+        "url"=> Configuration::$url.'domains/'.$data["id"].'/post/'.$ch,
+        "thumbnailUrl"=> $data["content"],
+        "postType"=> $data["postType"]);
+
+    $data_string = json_encode($data);
+    $rezult = $this->post_execute($data_string, 'domains/'.$data["id"].'/post');
+    if (!empty( $rezult["id"])){
+        return true;
+    }
+    else return false;
+}
+
+
+function post_execute($data, $method)
+{
+    $ch = curl_init(Configuration::$url.$method);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data))
+    );
+    $result = curl_exec($ch);
+    return json_decode($result, true);
+}
+
+
+$id='9';
+
+//$result1 = file_get_contents(Configuration::$url.'domains/'.$id.'/post/all', false);
+//echo $result1;
+$ch = curl_init(Configuration::$url.'domains/'.$id.'/post/all');
+//убрать все кроме цифр
+$ch = preg_replace('~[^0-9]+~','',$ch);
+echo $ch;
+$t= Configuration::$url.'domains/'.$id.'/post';
+$data = "test1";
+$data = array("title" => $data,
+    "content"=> "content_1",
+  "author"=> "author_1",
+  "excerpt"=> "excerpt_1",
+  "status"=> "private",
+  "postParent"=> $id,
+  "url"=> Configuration::$url.'domains/'.$id.'/post/1',
+  "thumbnailUrl"=> "thumbnailUrl_1",
+  "postType"=> "postType_1");
+
+print_r ($data);
+$data_string = json_encode($data);
+echo $data_string;
+$rezult = post_execute($data_string, 'domains/'.$id.'/post');
+print_r($rezult);
+$result1 = file_get_contents(Configuration::$url.'domains/'.$id.'/post/all', false);
+echo $result1;
+//$dd = get_domains();
+//echo $dd;
