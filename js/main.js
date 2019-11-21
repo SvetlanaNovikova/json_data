6bb81fc3faf0ad087979d04f1e86f41c9173d97c
Positiv <svetik-2011-1209@yandex.ru>
2019-11-21 23:28:24 +0300

    updated home page

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ var path_to_project = '/json_data';
 $(".all-domains").on("click", function () {
     $.get( path_to_project+"/view/all-domains.php", function( data ) {
         $('.main-info').html(data);
+        modal_show();
     })
 });
 
@@ -22,5 +23,17 @@ function add_domain() {
         })
     }
     return false;
+}
+
+$(".dropdown-menu li a").click(function(){
+    var selText = $(this).text();
+    $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+});
+
+function modal_show() {
+    if ($("#exampleModal").length > 0){
+        $("#exampleModal").modal('show');
+        $('.main-info').html("");
+    }
 }
 
--- view/all-domains.php
+++ view/all-domains.php
@@ -5,6 +5,30 @@ $model = new Class_methods();
 $domains = $model->get_domains('domains');
 $domains = json_decode($domains);
 
+if (empty($domains)){
+    echo '
+<div class="modal" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Ответ сервера</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Сервет не отвечает, пожалуста, подождите некоторое время и попробуййте снова.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+';
+    exit;
+}
+
 $table = '<table width=90% height=70% border=1>';
 foreach ($domains as $domain) {
     $table .= '<tr>'.'<td>' . $domain->id.'</td>'.'<td>' . $domain->domainName .'</td>'.'</tr>';
