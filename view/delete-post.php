81af30c5c46010913c22d130160b3ce527d3e2a4
Арина <8vedima8@gmail.com>
2019-12-02 21:04:02 +0300

    Merge branch 'master' of Z:\home\localhost\www\json_data with conflicts.

---
+++ view/delete-post.php
@@ -0,0 +1,43 @@
+<script src="js/main.js"></script>
+<?php
+include '../model/Class_methods.php';
+
+$model = new Class_methods();
+$data = array();
+$data["post_id"] = $_POST["post_id"];
+$posts = $model->delete_post($data);
+$str = "";
+if (empty($posts["title"])){
+    $str .= "Пост успешно удален<br>  " . $posts["title"];
+}
+else $str .= "Возникла ошибка при удалении поста:<br> " . $posts["error"]["message"];
+
+echo '
+<div class="modal" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Удаление поста</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ' . $str . '
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+';
+
+$(".delete-post").on("click", function () {
+    if ($("#delete-post")[0].checkValidity()) {
+        $.post(path_to_project + "/view/delete_post.php", {name:$("#delete-post").val()},  function (data) {
+            $('.main-info').html(data);
+            $("#exampleModal").modal('show');
+        })
+    }
+});
